f67aa0f6f4ec6c9f9aac1fde5dc7942ec9f9181d
IsaacZachary <isaaczachary18@gmail.com>
2026-03-06 07:52:38 +0300

    Fix router settings and .htaccess for subfolder compatibility

--- src/App.tsx
+++ src/App.tsx
@@ -15,7 +15,7 @@ const App = () => (
     <TooltipProvider>
       <Toaster />
       <Sonner />
-      <BrowserRouter>
+      <BrowserRouter basename="/recruitment">
         <Routes>
           <Route path="/" element={<JobListings />} />
           <Route path="/apply/:jobId" element={<ApplicationForm />} />
